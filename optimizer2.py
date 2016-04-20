391f3f369c6bd5ed1e9ad64b25aa3932325c1daa
Kevin Brez <kbrez@penny-newman.com>
2016-04-20 10:56:07 -0700

    pep8 standards

--- optimizer2.py
+++ optimizer2.py
@@ -5,7 +5,12 @@ import random
 ROWS = 5
 COLS = 6
 TYPES = 6
-COLORS = ["R", "B", "G", "Y", "P", "H"]
+COLORS = ["R",
+          "B",
+          "G",
+          "Y",
+          "P",
+          "H"]
 BOARD = [[random.choice(COLORS) for x in range(COLS)] for x in range(ROWS)]  # Randomizing the board
 
 
@@ -14,9 +19,9 @@ def create_empty_board():
     return [[0 for x in range(COLS)] for x in range(ROWS)]
 
 
-def print_board(Board):
-    """Function to visually print a 5x6 boaard."""
-    for row in Board:
+def print_board(board):
+    """Function to visually print a 5x6 board."""
+    for row in board:
         for orb in row:
             print(orb, end="")
         print("")
@@ -30,9 +35,10 @@ def print_board(Board):
 #	return {"type": typing, "count": count, "isRow": isRow}
 
 
-def max_combos(Board):
+def max_combos(board):
+    """This returns the total number of possible matches for each orb type"""
     big_board = []
-    for row in Board:
+    for row in board:
         for orb in row:
             big_board.append(orb)
     r_max = int(big_board.count("R") / 3)
@@ -41,20 +47,27 @@ def max_combos(Board):
     y_max = int(big_board.count("Y") / 3)
     p_max = int(big_board.count("P") / 3)
     h_max = int(big_board.count("H") / 3)
-    big_board = [r_max, b_max, g_max, y_max, p_max, h_max]
+    big_board = [r_max,
+                 b_max,
+                 g_max,
+                 y_max,
+                 p_max,
+                 h_max]
     return big_board
 
 
-def find_matches(Board):
+def find_matches(board):
+    """Using an existing board, this finds all orbs that currently count as a match.
+    Returns a board that only shows matched orbs.
+    """
     match_board = create_empty_board()
-    matches = 0
     #find horizontal matches first
     for i in range(0, ROWS):
         prev_1_orb = "X"
         prev_2_orb = "X"
         for j in range(0, COLS):
-            cur_orb = Board[i][j]
-            if (prev_1_orb == prev_2_orb and prev_2_orb == cur_orb and cur_orb != "X"):
+            cur_orb = board[i][j]
+            if prev_1_orb == prev_2_orb and prev_2_orb == cur_orb and cur_orb != "X":
                 match_board[i][j] = cur_orb
                 match_board[i][j-1] = cur_orb
                 match_board[i][j-2] = cur_orb
@@ -66,8 +79,8 @@ def find_matches(Board):
         prev_1_orb = "X"
         prev_2_orb = "X"
         for i in range(0, ROWS):
-            cur_orb = Board[i][j]
-            if (prev_1_orb == prev_2_orb and prev_2_orb == cur_orb and cur_orb != "X"):
+            cur_orb = board[i][j]
+            if prev_1_orb == prev_2_orb and prev_2_orb == cur_orb and cur_orb != "X":
                 match_board[i][j] = cur_orb
                 match_board[i-1][j] = cur_orb
                 match_board[i-2][j] = cur_orb
